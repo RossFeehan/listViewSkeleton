bfa8fce07c3dcc22ae69e05752011dcd14e0026b
rossfeehan <thafirstboss@yahoo.co.uk>
2015-12-03 12:55:41 +0000

    Display the 20 teams of the premier league 2015 in a list view with their badges and names

--- listViewSkeleton/app/src/main/java/com/ross/feehan/listviewskeleton/MainActivity.java
+++ listViewSkeleton/app/src/main/java/com/ross/feehan/listviewskeleton/MainActivity.java
@@ -1,37 +1,37 @@
 package com.ross.feehan.listviewskeleton;
 
 import android.app.Activity;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.google.common.primitives.Ints;
+
+import java.util.Arrays;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends Activity {
+
+    @Bind(R.id.teamListView) ListView teamListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        List<String> teamNames = Arrays.asList(getResources().getStringArray(R.array.premierLeagueTeams));
+        TypedArray teamBadges = getResources().obtainTypedArray(R.array.teamBadges);
+
+        TeamNameListViewAdapter teamNameLVAdapter = new TeamNameListViewAdapter(this, R.layout.team_custom_listview, teamNames, teamBadges);
+        teamListView.setAdapter(teamNameLVAdapter);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
---
+++ listViewSkeleton/app/src/main/java/com/ross/feehan/listviewskeleton/TeamNameListViewAdapter.java
@@ -0,0 +1,87 @@
+package com.ross.feehan.listviewskeleton;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Ross Feehan on 03/12/2015.
+ * Copyright Ross Feehan
+ */
+public class TeamNameListViewAdapter extends ArrayAdapter<String> {
+
+    private Context ctx;
+    private int resourceLayoutID;
+    private List<String> teamNames;
+    private TypedArray teamBadges;
+
+   //Constructor
+    public TeamNameListViewAdapter(Context context, int resource,  List<String>teamsNames, TypedArray teamBadges) {
+        super(context, resource);
+        this.ctx = context;
+        this.resourceLayoutID = resource;
+        this.teamNames = teamsNames;
+        this.teamBadges = teamBadges;
+    }
+
+     /*Set each row of the list view
+	 * @Params Int position - the number of the row that is being inflated
+	 * @Params View convertView  - the row
+	 * @Params ViewGroup parent - The rootview
+	 * @Return View row - the row that has been inflated
+	 * (non-Javadoc)
+	 * @see android.widget.ArrayAdapter#getView(int, android.view.View, android.view.ViewGroup)
+	 */
+     @Override
+     public View getView(int position, View convertView, ViewGroup parent){
+
+         View row = convertView;
+         TeamsHolder holder = null;
+
+         if(row == null){
+             LayoutInflater inflater = ((Activity)ctx).getLayoutInflater();
+             row = inflater.inflate(R.layout.team_custom_listview, parent, false);
+
+             holder = new TeamsHolder(row);
+         }
+         else{
+             holder = (TeamsHolder)row.getTag();
+         }
+
+         holder.teamNameTV.setText(teamNames.get(position));
+         holder.teamBadgeIV.setImageDrawable(teamBadges.getDrawable(position));
+         row.setTag(holder);
+         return row;
+     }
+
+    @Override
+    public int getCount(){
+        return teamNames.size();
+    }
+
+    /* Class that holds the layout of the view that we are inflating
+	 *
+	 */
+     static class TeamsHolder{
+
+        @Bind(R.id.teamBadgeIV) ImageView teamBadgeIV;
+        @Bind(R.id.teamNameTV) TextView teamNameTV;
+
+        public TeamsHolder(View view){
+            ButterKnife.bind(this, view);
+        }
+    }
+
+
+}
